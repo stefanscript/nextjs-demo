740db8e9a1db2e2b2827c32dee4df6021d6e42b5
Daniel Calleja <pigrsi.boat@gmail.com>
2021-02-18 15:35:53 +0000

    Added acceptance test for removing item

--- __tests__/acceptance/todo-list.test.tsx
+++ __tests__/acceptance/todo-list.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react";
+import {render, screen, within} from "@testing-library/react";
 import React from "react";
 import TodoListPage from "../../pages/todo-list";
 import userEvent from "@testing-library/user-event";
@@ -23,6 +23,30 @@ describe('Given I am on the todo-list', () => {
       const secondItem = screen.getByTestId(item2Id);
 
       expect(secondItem).toHaveTextContent(item2Text);
+    });
+
+    describe("And I click delete next to item 1", () => {
+      it("Then only item 2 should be in the list", () => {
+        render(<TodoListPage />);
+        const itemNameInput1 = 'item-name-input';
+        const item1Text = 'item 1';
+        const item2Text = 'item 2 latest';
+
+        const itemNameInput = screen.getByTestId(itemNameInput1);
+        const addItemButton = screen.getByRole('button', { name: "Add" });
+
+        userEvent.type(itemNameInput, item1Text);
+        userEvent.click(addItemButton);
+        userEvent.type(itemNameInput, item2Text);
+        userEvent.click(addItemButton);
+
+        const item1 = screen.getByTestId("list-item-1");
+        const removeItemButton = within(item1).getByRole('button', { name: "Remove" });
+
+        userEvent.click(removeItemButton);
+
+        expect(item1).not.toBeInTheDocument();
+      });
     });
   });
 });
